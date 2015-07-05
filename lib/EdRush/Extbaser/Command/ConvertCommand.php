cddb425fcfeec9be08f08a0a5b31d3ca149b70ca
Wolfram Eberius <edrush@posteo.de>
2015-07-05 14:46:28 +0200

    Entites added to extbase configuration

--- lib/EdRush/Extbaser/Command/ConvertCommand.php
+++ lib/EdRush/Extbaser/Command/ConvertCommand.php
@@ -7,18 +7,15 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
 use Doctrine\ORM\Tools\DisconnectedClassMetadataFactory;
 use Doctrine\ORM\Tools\Console\MetadataFilter;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
-
 use EdRush\Extbaser\ExtbaseExporter;
 
 /**
  * @todo Handle multiple relations from one table to another for import (Doctrine error "Property xy already declared, must be declared only once...").
  */
-
 class ConvertCommand extends \Doctrine\DBAL\Tools\Console\Command\ImportCommand
 {
     protected function configure()
@@ -27,36 +24,34 @@ class ConvertCommand extends \Doctrine\DBAL\Tools\Console\Command\ImportCommand
             ->setName('extbaser:convert')
             ->setDescription('Convert a database scheme to a TYPO3 Extbase Extension.')
             ->addArgument('dbName', InputArgument::REQUIRED, 'The database name.')
-			->addArgument('extensionKey', InputArgument::REQUIRED, 'The target extension key.')
-			
-			->addOption('dbUser', null, InputOption::VALUE_OPTIONAL, 'The database user.')
-			->addOption('dbPassword', null, InputOption::VALUE_OPTIONAL, 'The database password.')
+            ->addArgument('extensionKey', InputArgument::REQUIRED, 'The target extension key.')
+
+            ->addOption('dbUser', null, InputOption::VALUE_OPTIONAL, 'The database user.')
+            ->addOption('dbPassword', null, InputOption::VALUE_OPTIONAL, 'The database password.')
             ->addOption('dbHost', null, InputOption::VALUE_OPTIONAL, 'The database host.')
-			->addOption('dbPort', null, InputOption::VALUE_OPTIONAL, 'The database port.')
-			->addOption('dbDriver', null, InputOption::VALUE_OPTIONAL, 'The database driver.')
-			->addOption('exportPath', null, InputOption::VALUE_OPTIONAL, 'The path to export the target extension to.')
-			
-			->addOption('em', null, InputOption::VALUE_OPTIONAL, 'The entity manager to use for this command')
-			->addOption('filter', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A string pattern used to match entities that should be mapped.')
-			->addOption('force', null, InputOption::VALUE_NONE, 'Force to overwrite existing ExtensionBuilder.json.')
-		;
+            ->addOption('dbPort', null, InputOption::VALUE_OPTIONAL, 'The database port.')
+            ->addOption('dbDriver', null, InputOption::VALUE_OPTIONAL, 'The database driver.')
+            ->addOption('exportPath', null, InputOption::VALUE_OPTIONAL, 'The path to export the target extension to.')
+
+            ->addOption('em', null, InputOption::VALUE_OPTIONAL, 'The entity manager to use for this command')
+            ->addOption('filter', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A string pattern used to match entities that should be mapped.')
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Force to overwrite existing ExtensionBuilder.json.')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-    	$dbName = $input->getArgument('dbName');
-		$extensionKey = $input->getArgument('extensionKey');
-		
+        $dbName = $input->getArgument('dbName');
+        $extensionKey = $input->getArgument('extensionKey');
+
         //defaults
-        $dbUser = $input->getOption('dbUser') ? $input->getOption('dbUser'): 'root'; 
-		$dbPassword = $input->getOption('dbPassword') ? $input->getOption('dbPassword') : null;
-		$dbHost = $input->getOption('dbHost') ? $input->getOption('dbHost') : '127.0.0.1';
-		$dbPort = $input->getOption('dbPort') ? $input->getOption('dbPort') : null;
-		$dbDriver = $input->getOption('dbDriver') ? $input->getOption('dbDriver') : 'pdo_mysql';
-		$exportPath = $input->getOption('exportPath') ? $input->getOption('exportPath') : '.';
-		
-		$path =  $exportPath.'/'.$extensionKey;
-        
+        $dbUser = $input->getOption('dbUser') ? $input->getOption('dbUser') : 'root';
+        $dbPassword = $input->getOption('dbPassword') ? $input->getOption('dbPassword') : null;
+        $dbHost = $input->getOption('dbHost') ? $input->getOption('dbHost') : '127.0.0.1';
+        $dbPort = $input->getOption('dbPort') ? $input->getOption('dbPort') : null;
+        $dbDriver = $input->getOption('dbDriver') ? $input->getOption('dbDriver') : 'pdo_mysql';
+        $exportPath = $input->getOption('exportPath') ? $input->getOption('exportPath') : '.';
+
         $config = new \Doctrine\DBAL\Configuration();
 
         $connectionParams = array(
@@ -65,61 +60,48 @@ class ConvertCommand extends \Doctrine\DBAL\Tools\Console\Command\ImportCommand
             'password' => $dbPassword,
             'host' => $dbHost,
             'driver' => $dbDriver,
-            'port' => $dbPort
+            'port' => $dbPort,
         );
 
         $config = Setup::createAnnotationMetadataConfiguration(array('.'), false);
-		$em = EntityManager::create($connectionParams, $config);
-		
-		$em->getConfiguration()->setMetadataDriverImpl(
-		    new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
-		        $em->getConnection()->getSchemaManager()
-		    )
-		);
-		
-		$emName = $input->getOption('em');
-		$emName = $emName ? $emName : 'default';
-		
-		$exporter = new ExtbaseExporter();
-        
+        $em = EntityManager::create($connectionParams, $config);
+
+        $em->getConfiguration()->setMetadataDriverImpl(
+            new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
+                $em->getConnection()->getSchemaManager()
+            )
+        );
+
+        $emName = $input->getOption('em');
+        $emName = $emName ? $emName : 'default';
+
+        $exporter = new ExtbaseExporter();
+        $exporter->setExtensionKey($extensionKey);
+        $exporter->setPath($exportPath);
+        $exporter->setOverwriteExistingFiles($input->getOption('force'));
+
         $cmf = new DisconnectedClassMetadataFactory();
         $cmf->setEntityManager($em);
-		
-		$metadata = $cmf->getAllMetadata();
+
+        $metadata = $cmf->getAllMetadata();
         $metadata = MetadataFilter::filter($metadata, $input->getOption('filter'));
 
         if ($metadata) {
+            $exporter->setMetadata($metadata);
             $output->writeln(sprintf('Importing mapping information from "<info>%s</info>" entity manager', $emName));
-			
-			$filename = $path.'/'.ExtbaseExporter::PROJECT_FILE_NAME;
-			
-			if (!is_dir($dir = dirname($filename))) {
-                mkdir($dir, 0777, true);
-            }
-			
-			if (is_readable($filename) && !$input->getOption('force'))
-			{
-				$output->writeln(sprintf('File "<info>%s</info>" already existing, use --force to replace it.', $filename), 'ERROR');
-            	$output->writeln('', 'ERROR');
-            	return 1;
-			}
-			
-			$exporter->setExtensionKey($extensionKey);
-			$exporter->setMetadata($metadata);
-			
-			$json = $exporter->exportJson();
-			foreach ($exporter->getLogs() as $log) {
-				$output->writeln('--'.$log);
-			} 
-			
-			file_put_contents($filename, $json);
 
-            return 0;
+            $result = $exporter->exportJson();
+
+            foreach ($exporter->getLogs() as $log) {
+                $output->writeln('--'.$log);
+            }
+
+            return $result;
         } else {
             $output->writeln('Database does not have any mapping information.', 'ERROR');
             $output->writeln('', 'ERROR');
+
             return 1;
         }
-
     }
 }
--- lib/EdRush/Extbaser/ExtbaseExporter.php
+++ lib/EdRush/Extbaser/ExtbaseExporter.php
@@ -4,63 +4,127 @@ namespace EdRush\Extbaser;
 
 class ExtbaseExporter
 {
-	const PROJECT_FILE_NAME = 'ExtensionBuilder.json';
-	
-	/**
+    const PROJECT_FILE_NAME = 'ExtensionBuilder.json';
+
+    /**
      * @var array
      */
     private $metadata;
-	
-	/**
+
+    /**
      * @var string
      */
     private $extensionKey;
-	
-	/**
+
+    /**
      * @var array
      */
-	private $logs;
-	
-	public function exportJson()
-	{
-		foreach ($this->metadata as $class) 
-		{
-			$className = $class->name;
-			$this->logs[] = $className;
-        }
-	}
-	
-	public function getMetadata(){
-		return $this->metadata;
-	}
+    private $logs;
 
-	/**
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * @var bool
+     */
+    private $overwriteExistingFiles = false;
+
+    public function exportJson()
+    {
+        $filename = $this->path.'/'.$this->extensionKey.'/'.self::PROJECT_FILE_NAME;
+
+        if (!is_dir($dir = dirname($filename))) {
+            mkdir($dir, 0777, true);
+        }
+
+        if (is_readable($filename) && !$this->overwriteExistingFiles) {
+            $this->logs[] = sprintf('File "<info>%s</info>" already existing, use --force to replace it.', $filename);
+
+            return 1;
+        }
+
+        $configuration = array();
+
+        foreach ($this->metadata as $class) {
+        	$className = $class->name;
+			$this->logs[] = 'Processing '.$className;
+			
+            $module = array();
+			$module['config']['position'] = array(100,100);
+			$module['name'] = $className;
+
+            $configuration['modules'][] = $module;
+        }
+
+        file_put_contents($filename, json_encode($configuration, JSON_PRETTY_PRINT));
+
+        return 0;
+    }
+
+    public function getMetadata()
+    {
+        return $this->metadata;
+    }
+
+    /**
      * @param array $metadata
      */
-	public function setMetadata($metadata){
-		$this->metadata = $metadata;
-	}
+    public function setMetadata($metadata)
+    {
+        $this->metadata = $metadata;
+    }
 
-	public function getExtensionKey(){
-		return $this->extensionKey;
-	}
+    public function getExtensionKey()
+    {
+        return $this->extensionKey;
+    }
 
-	/**
+    /**
      * @param string $extensionKey
      */
-	public function setExtensionKey($extensionKey){
-		$this->extensionKey = $extensionKey;
-	}
-	
-	public function getLogs(){
-		return $this->logs;
-	}
+    public function setExtensionKey($extensionKey)
+    {
+        $this->extensionKey = $extensionKey;
+    }
 
-	/**
+    public function getLogs()
+    {
+        return $this->logs;
+    }
+
+    /**
      * @param array $logs
      */
-	public function setLogs($logs){
-		$this->logs = $logs;
-	}
-	
+    public function setLogs($logs)
+    {
+        $this->logs = $logs;
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    public function getOverwriteExistingFiles()
+    {
+        return $this->overwriteExistingFiles;
+    }
+
+    /**
+     * @param bool $overwriteExistingFiles
+     */
+    public function setOverwriteExistingFiles($overwriteExistingFiles)
+    {
+        $this->overwriteExistingFiles = $overwriteExistingFiles;
+    }
 }
