7311eef3ecccc3725edef1fea3abedda8591aa00
Wolfram Eberius <edrush@posteo.de>
2015-07-05 15:01:44 +0200

    Updated documentation and argument names

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# t3extbaser-lib
-The library to convert a database scheme to a TYPO3 Extbase Extension.
+# Extbaser
+Convert a database scheme to a TYPO3 extbase extension.
+
+Using defaults:
+```
+php bin/app.php extbaser:convert your_scheme target_extension_key
+```
+
+Configure your connection:
+```
+php bin/app.php extbaser:convert your_scheme target_extension_key --host=your_ip --user=your_user --password=your_password
+```
--- lib/EdRush/Extbaser/Command/ConvertCommand.php
+++ lib/EdRush/Extbaser/Command/ConvertCommand.php
@@ -23,15 +23,15 @@ class ConvertCommand extends \Doctrine\DBAL\Tools\Console\Command\ImportCommand
         $this
             ->setName('extbaser:convert')
             ->setDescription('Convert a database scheme to a TYPO3 Extbase Extension.')
-            ->addArgument('dbName', InputArgument::REQUIRED, 'The database name.')
-            ->addArgument('extensionKey', InputArgument::REQUIRED, 'The target extension key.')
+            ->addArgument('dbname', InputArgument::REQUIRED, 'The database name.')
+            ->addArgument('extensionkey', InputArgument::REQUIRED, 'The target extension key.')
 
-            ->addOption('dbUser', null, InputOption::VALUE_OPTIONAL, 'The database user.')
-            ->addOption('dbPassword', null, InputOption::VALUE_OPTIONAL, 'The database password.')
-            ->addOption('dbHost', null, InputOption::VALUE_OPTIONAL, 'The database host.')
-            ->addOption('dbPort', null, InputOption::VALUE_OPTIONAL, 'The database port.')
-            ->addOption('dbDriver', null, InputOption::VALUE_OPTIONAL, 'The database driver.')
-            ->addOption('exportPath', null, InputOption::VALUE_OPTIONAL, 'The path to export the target extension to.')
+            ->addOption('user', null, InputOption::VALUE_OPTIONAL, 'The database user.')
+            ->addOption('password', null, InputOption::VALUE_OPTIONAL, 'The database password.')
+            ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'The database host.')
+            ->addOption('port', null, InputOption::VALUE_OPTIONAL, 'The database port.')
+            ->addOption('driver', null, InputOption::VALUE_OPTIONAL, 'The database driver.')
+            ->addOption('path', null, InputOption::VALUE_OPTIONAL, 'The path to export the target extension to.')
 
             ->addOption('em', null, InputOption::VALUE_OPTIONAL, 'The entity manager to use for this command')
             ->addOption('filter', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A string pattern used to match entities that should be mapped.')
@@ -41,16 +41,16 @@ class ConvertCommand extends \Doctrine\DBAL\Tools\Console\Command\ImportCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $dbName = $input->getArgument('dbName');
-        $extensionKey = $input->getArgument('extensionKey');
+        $dbName = $input->getArgument('dbname');
+        $extensionKey = $input->getArgument('extensionkey');
 
         //defaults
-        $dbUser = $input->getOption('dbUser') ? $input->getOption('dbUser') : 'root';
-        $dbPassword = $input->getOption('dbPassword') ? $input->getOption('dbPassword') : null;
-        $dbHost = $input->getOption('dbHost') ? $input->getOption('dbHost') : '127.0.0.1';
-        $dbPort = $input->getOption('dbPort') ? $input->getOption('dbPort') : null;
-        $dbDriver = $input->getOption('dbDriver') ? $input->getOption('dbDriver') : 'pdo_mysql';
-        $exportPath = $input->getOption('exportPath') ? $input->getOption('exportPath') : '.';
+        $dbUser = $input->getOption('user') ? $input->getOption('user') : 'root';
+        $dbPassword = $input->getOption('password') ? $input->getOption('password') : null;
+        $dbHost = $input->getOption('host') ? $input->getOption('host') : '127.0.0.1';
+        $dbPort = $input->getOption('port') ? $input->getOption('port') : null;
+        $dbDriver = $input->getOption('driver') ? $input->getOption('driver') : 'pdo_mysql';
+        $exportPath = $input->getOption('path') ? $input->getOption('path') : '.';
 
         $config = new \Doctrine\DBAL\Configuration();
 
