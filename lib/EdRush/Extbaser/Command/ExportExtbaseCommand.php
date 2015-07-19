d0bb2253d24287582282f644ed0cefc0bdd1af25
Wolfram Eberius <edrush@posteo.de>
2015-07-19 20:48:05 +0200

    Support sharing of default command options

--- lib/EdRush/Extbaser/Command/ExportExtbaseCommand.php
+++ lib/EdRush/Extbaser/Command/ExportExtbaseCommand.php
@@ -15,32 +15,48 @@ use EdRush\Extbaser\ExtbaseExporter;
 
 class ExportExtbaseCommand extends Command
 {
+	/**
+	 * Get command options for all environments
+	 *
+	 * return array
+	 */
+	public static function getDefaultInputOptions()
+	{
+		$inputOptions = array(
+			new InputOption('path', null, InputOption::VALUE_OPTIONAL, 'The path to export the extension to', '.'),
+			new InputOption('filter', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A string pattern used to match entities that should be mapped'),
+			new InputOption('force', 'f', InputOption::VALUE_NONE, 'Roundtrip existing extension'),
+		);
+		 
+		return $inputOptions;
+	}
+	
     protected function configure()
     {
     	$this
 	    	->setName('extbaser:export')
-	    	->setDescription('Export an existing database schema to a TYPO3 Extbase Extension.')
+	    	->setDescription('Export an existing database schema to a TYPO3 Extbase Extension')
 	    	
-	    	->addArgument('dbname', InputArgument::REQUIRED, 'The database name.')
-	    	->addArgument('extension-key', InputArgument::REQUIRED, 'The target TYPO3 Extension key.')
-	    	
-	    	->addOption('path', null, InputOption::VALUE_OPTIONAL, 'The path to export the extension to.')
-	    	->addOption('filter', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'A string pattern used to match entities that should be mapped.')
-	    	->addOption('force', 'f', InputOption::VALUE_NONE, 'Roundtrip existing project.')
+	    	->addArgument('dbname', InputArgument::REQUIRED, 'The database you want to export')
+	    	->addArgument('extension-key', InputArgument::REQUIRED, 'The target TYPO3 Extension key')
 	    	
 	    	//db connection parameters
-	    	->addOption('user', 'u', InputOption::VALUE_OPTIONAL, 'The database user.')
-	    	->addOption('password', 'p', InputOption::VALUE_OPTIONAL, 'The database password.')
-	    	->addOption('host', null, InputOption::VALUE_OPTIONAL, 'The database host.')
-	    	->addOption('port', null, InputOption::VALUE_OPTIONAL, 'The database port.')
-	    	->addOption('driver', null, InputOption::VALUE_OPTIONAL, 'The database driver.')
+	    	->addOption('user', 'u', InputOption::VALUE_OPTIONAL, 'The database user')
+	    	->addOption('password', 'p', InputOption::VALUE_OPTIONAL, 'The database password')
+	    	->addOption('host', null, InputOption::VALUE_OPTIONAL, 'The database host')
+	    	->addOption('port', null, InputOption::VALUE_OPTIONAL, 'The database port')
+	    	->addOption('driver', null, InputOption::VALUE_OPTIONAL, 'The database driver')
     	;
+    	
+    	foreach (self::getDefaultInputOptions() as $inputOption)
+    	{
+    		$this->getDefinition()->addOption($inputOption);
+    	}
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dbName = $input->getArgument('dbname');
-        $extensionKey = $input->getArgument('extension-key');
 
         //db defaults
         $dbUser = $input->getOption('user') ? $input->getOption('user') : 'root';
@@ -49,8 +65,6 @@ class ExportExtbaseCommand extends Command
         $dbPort = $input->getOption('port') ? $input->getOption('port') : null;
         $dbDriver = $input->getOption('driver') ? $input->getOption('driver') : 'pdo_mysql';
         
-        $exportPath = $input->getOption('path') ? $input->getOption('path') : '.';
-
         $connectionParams = array(
             'dbname' => $dbName,
             'user' => $dbUser,
@@ -73,8 +87,8 @@ class ExportExtbaseCommand extends Command
         $cmf->setEntityManager($em);
 
         $exporter = new ExtbaseExporter($cmf);
-        $exporter->setExtensionKey($extensionKey);
-        $exporter->setPath($exportPath);
+        $exporter->setExtensionKey($input->getArgument('extension-key'));
+        $exporter->setPath($input->getOption('path'));
         $exporter->setOverwriteExistingFiles($input->getOption('force'));
         $exporter->setFilter($input->getOption('filter'));
         
